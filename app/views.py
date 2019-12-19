4fa410a9601e47648c845ba2c30932b4e04add1a
ekkopy <thiagognlcn@gmail.com>
2019-12-19 14:23:55 -0300

    finish crawler :books:

--- app/views.py
+++ app/views.py
@@ -8,7 +8,8 @@ import requests
 def crawler(request, value):
     url = f"https://michaelis.uol.com.br/moderno-portugues/busca/portugues-brasileiro/{value}"
     response = requests.get(url, timeout=5)
-    content = BeautifulSoup(response.text, "html.parser")
-    word = content.find(class_="verbete bs-component")
-    new_cont = word.find_all('eu')
-    return HttpResponse(new_cont)
+    content = BeautifulSoup(response.content, 'html.parser')
+    for parse_tree in content.find_all('div', attrs={'class': 'verbete bs-component'}):
+        result = parse_tree.contents[9]
+
+    return render(request, "index.html", {"value": value , "result": result})
